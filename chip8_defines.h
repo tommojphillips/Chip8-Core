88998e40a8938d97413c8b3bb35689cca1d39520
Tommo J. Armytage <tommojphillips@gmail.com>
2025-01-20 18:22:18 +1100

    modified defines

--- chip8_defines.h
+++ chip8_defines.h
@@ -9,13 +9,13 @@
  Originally a pixel takes up 1 byte but only uses 1 bit of data,
  there is 8 bits in a byte, so if dynamic memory is tight you can
  enable this to save 1792 bytes! */
-//#define CHIP8_SHRINK_VIDEO_RAM
+//#define CHIP8_SHRINK_DISPLAY_RAM
 
 #define CHIP8_MNEMONICS
 
 #ifdef ARDUINO
-#ifndef CHIP8_SHRINK_VIDEO_RAM
-#define CHIP8_SHRINK_VIDEO_RAM
+#ifndef CHIP8_SHRINK_DISPLAY_RAM
+#define CHIP8_SHRINK_DISPLAY_RAM
 #endif
 #undef CHIP8_MNEMONICS
 #endif
